074079aebf779ef3ce593a98155c6ce78727bdd3
Daniel Smith <dan@getabetterpic.com>
2016-06-02 23:22:34 -0400

    Add rocket model and route

---
+++ app/models/rocket.js
@@ -0,0 +1,15 @@
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+import { hasMany } from 'ember-data/relationships';
+
+export default Model.extend({
+  name: attr('string'),
+  manufacturer: attr('string'),
+  length: attr('number'),
+  diameter: attr('number'),
+  centerOfPressure: attr('number'),
+  centerOfGravity: attr('number'),
+  dragCoefficient: attr('number'),
+  stages: attr('number'),
+  flights: hasMany('flight', { async: true })
+});
