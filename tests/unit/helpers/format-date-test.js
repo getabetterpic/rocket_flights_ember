e7a7a734a9108fc86a6536ff4ee466572eb4b060
getabetterpic <dan@danandjensmith.com>
2016-06-09 18:25:22 -0400

    Add format-date helper

---
+++ tests/unit/helpers/format-date-test.js
@@ -0,0 +1,10 @@
+import { formatDate } from 'rocket-flights-ember/helpers/format-date';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | format date');
+
+// Replace this with your real tests.
+test('it correctly formats a date object', function(assert) {
+  let result = formatDate(Date('2016', '06', '09'), { format: 'LL' });
+  assert.equal(result, 'June 9, 2016');
+});
