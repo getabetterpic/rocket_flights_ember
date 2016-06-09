4d0c142d9a8669bc26488706753cdfd87e3c0987
getabetterpic <dan@danandjensmith.com>
2016-06-09 17:01:23 -0400

    Correct how fonts get imported

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -12,9 +12,9 @@ module.exports = function(defaults) {
   app.import('bower_components/Materialize/dist/js/materialize.js');
 
   const destDir = 'fonts/roboto';
-  app.import('bower_components/Materialize/fonts/roboto/Roboto-Regular.woff2', { destDir });
-  app.import('bower_components/Materialize/fonts/roboto/Roboto-Regular.woff', { destDir });
-  app.import('bower_components/Materialize/fonts/roboto/Roboto-Regular.ttf', { destDir });
+  app.import('bower_components/Materialize/fonts/roboto/Roboto-Regular.woff2', { destDir: destDir });
+  app.import('bower_components/Materialize/fonts/roboto/Roboto-Regular.woff', { destDir: destDir });
+  app.import('bower_components/Materialize/fonts/roboto/Roboto-Regular.ttf', { destDir: destDir });
 
 
   // Use `app.import` to add additional libraries to the generated
