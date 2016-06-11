a0eaf445d1982c2b277d781f82029905e1a9add9
getabetterpic <dan@danandjensmith.com>
2016-06-10 20:31:06 -0400

    Change flights/new route and template
     to correctly display and save model

--- app/routes/flights/new.js
+++ app/routes/flights/new.js
@@ -2,12 +2,26 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return this.store.find('rocket', params.rocket_id).then((rocket) => {
+    if (params.rocket_id) {
+      return this.store.find('rocket', params.rocket_id).then((rocket) => {
+        const dt = new Date();
+        return this.store.createRecord('flight', {
+          rocket: rocket,
+          flightDate: dt
+        });
+      });
+    } else {
       const dt = new Date();
       return this.store.createRecord('flight', {
-        rocket: rocket,
         flightDate: dt
       });
-    });
+    }
+  },
+  actions: {
+    saveFlight(model) {
+      model.save().then(() => {
+        Materialize.toast("Flight saved", 1500);
+      });
+    }
   }
 });
