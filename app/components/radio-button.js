5a5a0c2fccfac3ff88c4a669d097e1c6ef43f411
getabetterpic <dan@danandjensmith.com>
2016-06-10 20:32:08 -0400

    Add radio button component

---
+++ app/components/radio-button.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+const { computed } = Ember;
+
+export default Ember.Component.extend({
+  tagName: 'input',
+  type: 'radio',
+  attributeBindings: ['type', 'checked'],
+  checked: computed('value', 'groupValue', function() {
+    return this.get('value') === this.get('groupValue');
+  }).readOnly(),
+
+  change() {
+    let value = this.get('value');
+    let groupValue = this.get('groupValue');
+
+    if (groupValue !== value) {
+      this.set('groupValue', value);
+    }
+  }
+});
