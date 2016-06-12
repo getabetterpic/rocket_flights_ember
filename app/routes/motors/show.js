18026631cc812ebb7ff57802b5f3c8972732345a
getabetterpic <dan@danandjensmith.com>
2016-06-12 17:38:12 -0400

    Add saveMotor action to show route

--- app/routes/motors/show.js
+++ app/routes/motors/show.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    saveMotor(motor) {
+      motor.save().then((motor) => {
+        Materialize.toast("Motor saved", 1500);
+      }).catch((error) => {
+        error.errors.forEach((error) => {
+          Materialize.toast(error.detail.capitalize(), 3000);
+        });
+      });
+    }
+  }
 });
