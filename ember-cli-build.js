99debc7cb83226e0d036b58646be64ae736afe9d
getabetterpic <dan@danandjensmith.com>
2016-06-12 08:06:49 -0400

    Add additional Roboto weights

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -17,6 +17,18 @@ module.exports = function(defaults) {
   app.import('bower_components/Materialize/fonts/roboto/Roboto-Regular.woff', { destDir: destDir });
   app.import('bower_components/Materialize/fonts/roboto/Roboto-Regular.ttf', { destDir: destDir });
 
+  app.import('bower_components/Materialize/fonts/roboto/Roboto-Medium.woff2', { destDir: destDir });
+  app.import('bower_components/Materialize/fonts/roboto/Roboto-Medium.woff', { destDir: destDir });
+  app.import('bower_components/Materialize/fonts/roboto/Roboto-Medium.ttf', { destDir: destDir });
+
+  app.import('bower_components/Materialize/fonts/roboto/Roboto-Light.woff2', { destDir: destDir });
+  app.import('bower_components/Materialize/fonts/roboto/Roboto-Light.woff', { destDir: destDir });
+  app.import('bower_components/Materialize/fonts/roboto/Roboto-Light.ttf', { destDir: destDir });
+
+  app.import('bower_components/Materialize/fonts/roboto/Roboto-Thin.woff2', { destDir: destDir });
+  app.import('bower_components/Materialize/fonts/roboto/Roboto-Thin.woff', { destDir: destDir });
+  app.import('bower_components/Materialize/fonts/roboto/Roboto-Thin.ttf', { destDir: destDir });
+
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
