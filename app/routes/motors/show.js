2e9acd58a8604bce453e6655915450cb8973258a
getabetterpic <dan@danandjensmith.com>
2016-06-13 06:53:41 -0400

    Finish new and show routes

--- app/routes/motors/show.js
+++ app/routes/motors/show.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   actions: {
     saveMotor(motor) {
-      motor.save().then((motor) => {
+      motor.save().then(() => {
         Materialize.toast("Motor saved", 1500);
       }).catch((error) => {
         error.errors.forEach((error) => {
--- tests/integration/components/motor-data-test.js
+++ tests/integration/components/motor-data-test.js
@@ -5,20 +5,11 @@ moduleForComponent('motor-data', 'Integration | Component | motor data', {
   integration: true
 });
 
-test('it renders', function(assert) {
+test('it saves a motor', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
   this.render(hbs`{{motor-data}}`);
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#motor-data}}
-      template block text
-    {{/motor-data}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.ok(this.$('form'));
 });
