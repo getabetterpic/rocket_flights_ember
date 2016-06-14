499215f182cc5386de4c9e528607ba8fad4b85b5
getabetterpic <dan@danandjensmith.com>
2016-06-14 17:08:16 -0400

    Add flightMotors to flight model

--- app/models/flight.js
+++ app/models/flight.js
@@ -1,10 +1,11 @@
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
-import { belongsTo } from 'ember-data/relationships';
+import { belongsTo, hasMany } from 'ember-data/relationships';
 
 export default Model.extend({
   successful: attr('boolean'),
   notes: attr('string'),
   flightDate: attr('date'),
-  rocket: belongsTo('rocket')
+  rocket: belongsTo('rocket'),
+  flightMotors: hasMany('flightMotor')
 });
