f585e9fb6b7a3b5d68969eda516b739d141c48c4
getabetterpic <dan@danandjensmith.com>
2016-06-24 17:28:13 -0400

    Correct computed properties on userRocket model

--- app/models/user-rocket.js
+++ app/models/user-rocket.js
@@ -1,14 +1,13 @@
 import Model from 'ember-data/model';
 import { belongsTo } from 'ember-data/relationships';
-import { computed } from 'ember';
 
 export default Model.extend({
   user: belongsTo('user'),
   rocket: belongsTo('rocket'),
-  rocketName: computed('rocket.name', 'rocket.manufacturer', function() {
+  rocketName: Ember.computed('rocket.name', 'rocket.manufacturer', function() {
     return `${this.get('rocket.manufacturer')} ${this.get('rocket.name')}`;
   }),
-  rocketFlightsLength: computed('rocket.flights.@each', function() {
+  rocketFlightsLength: Ember.computed('rocket.flights.@each', function() {
     return this.get('rocket.flights.length');
   })
 });
