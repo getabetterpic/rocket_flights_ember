5f55ab12dc8ccbf0117f07e2058e880141177697
getabetterpic <dan@danandjensmith.com>
2016-06-24 17:18:47 -0400

    Add rocketName and rocketFlightsLength properties

--- app/models/user-rocket.js
+++ app/models/user-rocket.js
@@ -1,7 +1,14 @@
 import Model from 'ember-data/model';
 import { belongsTo } from 'ember-data/relationships';
+import { computed } from 'ember';
 
 export default Model.extend({
   user: belongsTo('user'),
-  rocket: belongsTo('rocket')
+  rocket: belongsTo('rocket'),
+  rocketName: computed('rocket.name', 'rocket.manufacturer', function() {
+    return `${this.get('rocket.manufacturer')} ${this.get('rocket.name')}`;
+  }),
+  rocketFlightsLength: computed('rocket.flights.@each', function() {
+    return this.get('rocket.flights.length');
+  })
 });
