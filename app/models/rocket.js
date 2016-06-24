e4769154c9a84687af91ca67544c07efe538864a
getabetterpic <dan@danandjensmith.com>
2016-06-24 17:27:37 -0400

    Add userRockets to rocket model

--- app/models/rocket.js
+++ app/models/rocket.js
@@ -5,5 +5,6 @@ import { hasMany } from 'ember-data/relationships';
 export default Model.extend({
   name: attr('string'),
   manufacturer: attr('string'),
-  flights: hasMany('flight')
+  flights: hasMany('flight'),
+  userRockets: hasMany('userRocket')
 });
