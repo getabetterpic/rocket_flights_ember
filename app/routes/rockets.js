3db8af289775c9e61f9b3d543dd834e9783a86ba
getabetterpic <dan@danandjensmith.com>
2016-06-24 18:26:04 -0400

    Only show tabs on rockets if session is authenticated

--- app/routes/rockets.js
+++ app/routes/rockets.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  session: Ember.inject.service(),
+
+  setupController(controller, model) {
+    this._super(controller, model);
+    controller.set('session', this.get('session'));
+  }
 });
