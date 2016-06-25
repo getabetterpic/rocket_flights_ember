e1f5ccab711f5dfad0703e107ec2d5ee9eebe326
getabetterpic <dan@danandjensmith.com>
2016-06-25 00:28:32 -0400

    Add Google Analytics tracking

--- app/app.js
+++ app/app.js
@@ -3,6 +3,16 @@ import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
+Ember.Router.reopen({
+  notifyGoogleAnalytics: Ember.on('didTransition', function() {
+    if (!ga) { return; }
+		return ga('send', 'pageview', {
+			page: this.get('url'),
+			title: this.get('url')
+		});
+  })
+});
+
 let App;
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
