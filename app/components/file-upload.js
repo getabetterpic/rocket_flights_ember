34a815a42a74e400921e0ccfa677b28ece96aa3a
getabetterpic <dan@danandjensmith.com>
2016-07-03 16:02:22 -0400

    Upload image then add to store

--- app/components/file-upload.js
+++ app/components/file-upload.js
@@ -1,14 +1,28 @@
 import EmberUploader from 'ember-uploader';
 import ENV from 'rocket-flights-ember/config/environment';
+import Ember from 'ember';
 
 export default EmberUploader.FileField.extend({
+  session: Ember.inject.service(),
+
   filesDidChange(files) {
-    const uploader = EmberUploader.Uploader.create({
-      url: `${ENV.HOST}${this.get('url')}`
+    let uploader;
+    this.get('session').authorize('authorizer:auth0', (headerName, headerValue) => {
+      let headers = {};
+      headers[headerName] = headerValue;
+      uploader = EmberUploader.Uploader.create({
+        url: `${ENV.HOST}${this.get('url')}`,
+        ajaxSettings: {
+          headers: headers
+        }
+      });
     });
 
     if (!Ember.isEmpty(files)) {
-      uploader.upload(files[0], { rocket: { id: this.get('rocket.id') } });
+      uploader.upload(files[0], { rocket_id: this.get('rocket.id') }).then((image) => {
+        Ember.$('#upload-modal').closeModal();
+        this.addImageToStore(image);
+      });
     }
   }
 });
